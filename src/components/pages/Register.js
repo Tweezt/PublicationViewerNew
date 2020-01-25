ca74953eda08b83f433ee55a8292b039b3d65fca
MrFreesher <artur260@op.pl>
2020-01-25 23:32:37 +0100

    Add simple register form and request

--- src/components/pages/Register.js
+++ src/components/pages/Register.js
@@ -1,34 +1,97 @@
 import React, { useState } from "react";
-import { Button, FormGroup, FormControl, FormLabel } from "react-bootstrap";
+import {
+  Button,
+  FormGroup,
+  FormControl,
+  FormLabel,
+  Alert
+} from "react-bootstrap";
 import "./Register.css";
+import Axios from "axios";
 
 export default function Register(props) {
-  const [email, setEmail] = useState("");
+  const [login, setLogin] = useState("");
   const [password, setPassword] = useState("");
   const [passwordConfirm, setPasswordConfirm] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [isMessage, setMessage] = useState(false);
+  const [message, setMessageContent] = useState("");
 
   function validateForm() {
-    return email.length > 0 && password.length > 0;
+    return (
+      login.length > 0 &&
+      password.length > 0 &&
+      passwordConfirm > 0 &&
+      firstName.length > 0 &&
+      lastName.length > 0
+    );
   }
 
+  function comparePasswords() {
+    if (password.length === passwordConfirm.length) {
+      if (password !== passwordConfirm) {
+        setMessage(true);
+        setMessageContent("Password aren't the same");
+        return false;
+      } else {
+        setMessage(false);
+        setMessageContent();
+        return true;
+      }
+    } else {
+      return false;
+    }
+  }
+  function clearForm() {
+    setFirstName("");
+    setLastName("");
+    setLogin("");
+    setPassword("");
+    setPasswordConfirm("");
+  }
   function handleSubmit(event) {
     event.preventDefault();
+    if (comparePasswords()) {
+      const userObj = {
+        login,
+        firstName,
+        lastName,
+        password
+      };
+      Axios.post("http://publisher.freesher.ct8.pl/user/signup", userObj)
+        .then(res => res.data)
+        .then(res => {
+          console.log(res);
+          setMessage(true);
+          setMessageContent(res.message);
+          clearForm();
+        })
+        .catch(err => {
+          console.log(err.response.data.message);
+          setMessage(true);
+          setMessageContent(err.response.data.message);
+        });
+    }
   }
 
   return (
     <div className="Register">
+      <Alert variant="primary" show={isMessage}>
+        {message}
+      </Alert>
       <form onSubmit={handleSubmit}>
-        <FormGroup controlId="email" bssize="large">
-          <FormLabel>Email</FormLabel>
+        <FormGroup controlId="login" bssize="large">
+          <FormLabel>Login*</FormLabel>
           <FormControl
             autoFocus
-            type="email"
-            value={email}
-            onChange={e => setEmail(e.target.value)}
+            type="text"
+            value={login}
+            onChange={e => setLogin(e.target.value)}
           />
         </FormGroup>
         <FormGroup controlId="password" bssize="large">
-          <FormLabel>Password</FormLabel>
+          <FormLabel>Password*</FormLabel>
           <FormControl
             value={password}
             onChange={e => setPassword(e.target.value)}
@@ -36,13 +99,32 @@ export default function Register(props) {
           />
         </FormGroup>
         <FormGroup controlId="passwordConfirm" bssize="large">
-          <FormLabel>Repeat Password</FormLabel>
+          <FormLabel>Repeat Password*</FormLabel>
           <FormControl
             value={passwordConfirm}
             onChange={e => setPasswordConfirm(e.target.value)}
             type="password"
           />
         </FormGroup>
+        <FormGroup controlId="firstName" bssize="large">
+          <FormLabel>First Name*</FormLabel>
+          <FormControl
+            autoFocus
+            type="text"
+            value={firstName}
+            onChange={e => setFirstName(e.target.value)}
+          />
+        </FormGroup>
+        <FormGroup controlId="lastName" bssize="large">
+          <FormLabel>Last Name*</FormLabel>
+          <FormControl
+            autoFocus
+            type="text"
+            value={lastName}
+            onChange={e => setLastName(e.target.value)}
+          />
+        </FormGroup>
+        <FormLabel>*Pole wymagane</FormLabel>
         <Button block bssize="large" disabled={!validateForm()} type="submit">
           Register
         </Button>
