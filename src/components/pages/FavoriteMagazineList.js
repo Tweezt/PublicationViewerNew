f8a9e974c462366de84d74052ea07a15354fb060
MrFreesher <artur260@op.pl>
2020-01-27 12:20:14 +0100

    Add favorites to pdf

--- src/components/pages/FavoriteMagazineList.js
+++ src/components/pages/FavoriteMagazineList.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Magazines from "../Magazines";
 import PopupModal from "../PopupModal";
 import Axios from "axios";
+import { Button } from "react-bootstrap";
 class FavoriteMagazineList extends Component {
   constructor(props) {
     super(props);
@@ -51,10 +52,61 @@ class FavoriteMagazineList extends Component {
         console.error(err);
       });
   };
+  makePdf = () => {
+    let thead = document.querySelectorAll("thead tr th");
+
+    let tbody = document.querySelector("table").rows;
+
+    //const theadT = thead.split("<th>");
+
+    var style = "<style>";
+    style = style + "table {width: 100%;font: 17px Calibri;}";
+    style =
+      style +
+      "table, th, td {border: solid 1px #DDD; border-collapse: collapse;";
+    style = style + "padding: 2px 3px;text-align: center;}";
+    style = style + "</style>";
+    var win = window.open("", "", "height=700,width=700");
+    win.document.write("<html><head>");
+    win.document.write("<title>Profile</title>"); // <title> FOR PDF HEADER.
+    win.document.write(style); // ADD STYLE INSIDE THE HEAD TAG.
+    win.document.write("</head>");
+    win.document.write("<body>");
+    win.document.write("<h2>Lista ulubionych czasopism naukowych</h2>");
+    win.document.write(`<h4> Wygenerowano ${new Date().toLocaleString()}</h4>`);
+
+    win.document.write("<table>");
+    win.document.write("<thead>");
+    win.document.write("<tr>");
+    for (let i = 0; i < thead.length - 1; i++) {
+      win.document.write(`<th>${thead[i].innerHTML}</th>`);
+    }
+    win.document.write("</tr>");
+    win.document.write("<tbody>");
+    for (let i = 1; i < tbody.length; i++) {
+      win.document.write("<tr>");
+      for (let j = 0; j < tbody[i].cells.length - 1; j++) {
+        win.document.write(`<td>${tbody[i].cells[j].innerHTML}</td>`);
+      }
+      win.document.write("</tr>");
+    }
+    win.document.write("</tbody>");
+    // THE TABLE CONTENTS INSIDE THE BODY TAG.
+    win.document.write("</table>");
+    win.document.write("</body></html>");
+
+    win.document.close();
+    win.print();
+  };
   render() {
     return (
       <React.Fragment>
-        <h2>Favorites Magazines</h2>
+        <header style={headerStyle}>
+          <h2>Favorites Magazines</h2>
+          <Button type="button" onClick={() => this.makePdf()}>
+            Make PDF
+          </Button>
+        </header>
         <Magazines
           magazines={this.state.magazines}
           showMagazinePopUp={this.showMagazinePopUp}
@@ -64,4 +116,9 @@ class FavoriteMagazineList extends Component {
     );
   }
 }
+const headerStyle = {
+  display: "flex",
+  flexDireciton: "row",
+  justifyContent: "space-between"
+};
 export default FavoriteMagazineList;
